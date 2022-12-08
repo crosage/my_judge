50825bd20aa48a0ecba72c62069317a386305792
crosa <15132844856@163.com>
2022-12-08 19:31:44 +0800

    改回去

--- my_judge/dealCompile/dealCompile.cpp
+++ my_judge/dealCompile/dealCompile.cpp
@@ -104,9 +104,9 @@ int main(int argc,char *argv[])
         tmp=tmp+" 2>&1";
         json info;
         if(flag)
-            info["compile_info"]="error";
+            info["compile_info"]="compile_error";
         else 
-            info["compile_info"]="pass";
+            info["compile_info"]="success";
         info["traceback"]=exec(tmp.c_str());
         cout<<info<<endl;
     }
